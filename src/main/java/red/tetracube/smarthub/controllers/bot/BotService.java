39789faa156b0501df3d0b713ba1e2706c4c6e12
Davide-Di-Do <davide.dcube@gmail.com>
2021-05-25 10:30:07 +0200

    📕 Added some logs

--- src/main/java/red/tetracube/smarthub/controllers/bot/BotService.java
+++ src/main/java/red/tetracube/smarthub/controllers/bot/BotService.java
@@ -1,10 +1,11 @@
 package red.tetracube.smarthub.controllers.bot;
 
 import io.smallrye.mutiny.Uni;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import red.tetracube.smarthub.controllers.bot.payloads.BotDeviceFeatureItem;
 import red.tetracube.smarthub.controllers.bot.payloads.DeviceFeatureAction;
 import red.tetracube.smarthub.data.entities.Action;
-import red.tetracube.smarthub.data.entities.Feature;
 import red.tetracube.smarthub.data.repositories.ActionRepository;
 import red.tetracube.smarthub.data.repositories.DeviceRepository;
 import red.tetracube.smarthub.data.repositories.FeatureRepository;
@@ -26,17 +27,23 @@ public class BotService {
     @Inject
     ActionRepository actionRepository;
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(BotService.class);
+
     public Uni<List<BotDeviceFeatureItem>> getDeviceFeatures() {
         return deviceRepository.getDevices()
-                .flatMap(device ->
-                        featureRepository.getFeaturesByDevice(device.getId())
-                                .map(feature ->
-                                        new BotDeviceFeatureItem(
-                                                device.getName(),
-                                                feature.getName(),
-                                                device.getColorCode()
-                                        )
-                                )
+                .flatMap(device -> {
+                            LOGGER.info("Found device \"{}\", getting its features", device.getName());
+                            return featureRepository.getFeaturesByDevice(device.getId())
+                                    .map(feature -> {
+                                                LOGGER.info("Found feature \"{}\"", feature.getName());
+                                                return new BotDeviceFeatureItem(
+                                                        device.getName(),
+                                                        feature.getName(),
+                                                        device.getColorCode()
+                                                );
+                                            }
+                                    );
+                        }
                 )
                 .collect()
                 .asList();
@@ -44,18 +51,25 @@ public class BotService {
 
     public Uni<Optional<DeviceFeatureAction>> getDeviceFeatureActions(String deviceName,
                                                                       String featureName) {
+        LOGGER.info("Searching for device with name \"{}\"", deviceName);
         return deviceRepository.getDeviceByName(deviceName)
                 .flatMap(optionalDevice -> {
                     if (optionalDevice.isEmpty()) {
+                        LOGGER.warn("Not found, returning empty object");
                         return Uni.createFrom().item(Optional.empty());
                     }
+                    LOGGER.info("Found device id \"{}\"", optionalDevice.get().getId());
+                    LOGGER.info("Searching for related feature name \"{}\"", featureName);
                     return featureRepository.getFeatureByDeviceAndName(optionalDevice.get().getId(), featureName)
                             .flatMap(optionalFeature -> {
                                 if (optionalFeature.isEmpty()) {
+                                    LOGGER.warn("Not found, returning empty object");
                                     return Uni.createFrom().item(Optional.empty());
                                 }
+                                LOGGER.info("Found, now searching for the related actions");
                                 var actionsNames = actionRepository.getFeatureActions(optionalFeature.get().getId())
                                         .map(Action::getName)
+                                        .invoke(action -> LOGGER.info("Found action \"{}\"", action))
                                         .collect().asList();
                                 var deviceFeatureActionsUni = actionsNames.map(actions -> new DeviceFeatureAction(
                                         optionalDevice.get().getName(),
