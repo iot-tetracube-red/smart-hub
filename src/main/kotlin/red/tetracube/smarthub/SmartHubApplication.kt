c9f3cbbcedce55938173d10e914a4c6433c1ccd8
Davide-Di-Do <davide.dcube@gmail.com>
2021-06-25 00:02:44 +0200

    🏁 Created project

---
+++ src/main/kotlin/red/tetracube/smarthub/SmartHubApplication.kt
@@ -0,0 +1,11 @@
+package red.tetracube.smarthub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmartHubApplication
+
+fun main(args: Array<String>) {
+	runApplication<SmartHubApplication>(*args)
+}
